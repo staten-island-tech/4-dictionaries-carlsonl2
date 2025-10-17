6f284585acea7d3c4cac56420a6658c56a430027
carlsonl2 <carlsonl2@nycstudents.net>
2025-10-17 05:45:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-Airpods = {
+Store = [{
 "name" : "Airpods",
 "Price" : 199.99,
 "Store" : "Apple"
-}
+},
 
-IPhone = {
-"Name" : "IPhone",
-"Price" : 1199.99,
-"Store" : "Apple"
-}
+{
+    "Name" : "IPhone",
+    "Price" : 1199.99,
+    "Store" : "Apple"
+},
 
-IPad = {
-""
-}
+{
+    "Name" : "IPad",
+    "Price" : 799.99,
+    "Store" : "Apple"
+}]
+
+print(Store[1]["Price"])
