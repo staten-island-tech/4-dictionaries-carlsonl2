e1c26aa79712abae5d6a807dbab70a9b91589056
carlsonl2 <carlsonl2@nycstudents.net>
2025-10-27 06:25:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-'''Store = [{
-"Name" : "Airpods",
-"Price" : 199.99,
-"Store" : "Apple"
+Store = [{
+    "Name" : "Airpods",
+    "Price" : 199.99,
+    "Store" : "Apple"
 },
 
 {
@@ -21,6 +21,8 @@ print("Welcome to the Store!")
 for index, item in enumerate(Store):
    print(f"{index} : {item['Name']}, ${item['Price']}, {item['Store']}")
 
+
+
     #the f string allows the code to output multiple values in a dictionary of a list of dictionaries
 cart = []
 subtotal = 0
@@ -28,50 +30,16 @@ done = False
 
 while done == False:
     choose = input("What would you like to buy?")
-    for item in Store:
-        if choose == [item]['Name']:
-            cart.append([item]['Name'])
+    for item in range(Store):
+        if choose == Store[item]["Name"]:
+            cart.append(Store[item]["Name"])
             subtotal = subtotal + Store[item]['Price']
-    print(f"{cart}")
-    print(subtotal)'''
-
-'''def late(y):
-    waiting = 0 
-    late = 0
-    students = y + late
-    set = ["take", "take", "serve", "take", "serve", "serve", "close"]
-    while students < 999:
-        for i in range(7): 
-            if set[i] == "take":
-                waiting == waiting + 1 and late == late + 1
-            elif set[i] == "serve":
-                waiting == waiting - 1
-            elif set[i] == "close":
-                print(f"{late, waiting, students}")
-late(23)'''
-
-'''set = ["take", "take", "serve", "take", "serve", "serve", "close"]
-for i in range(6):
-    print(set[i])'''
-
-'''def late_students(x,y):
-    waiting = 0
-    late = 0
-    while x < 999:
-        for i in range(y):
-            if y[i] == "take":
-                waiting = waiting + 1 
-                late = late + 1
-            elif y[1] == "serve":
-                waiting = waiting - 1
-            elif y[i] == "close":
-                nextnumber = x + late
-        print(f"{late, waiting, nextnumber}")
-        break
-late_students(23, ["take", "take", "serve", "take", "serve", "serve", "close"])'''
+    print(cart)
+    
 
 
-def late(x, y):
+
+'''def late(x, y):
     late = 0
     waiting = 0
     for i in range(7):
@@ -83,5 +51,5 @@ def late(x, y):
         elif y[i] == "close":
             nextnumber = x + late
             print(late, waiting, nextnumber)
-late(23, ["take", "take", "serve", "take", "serve", "serve", "close"])
+late(23, ["take", "take", "serve", "take", "serve", "serve", "close"])'''
         
